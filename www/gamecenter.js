9063240c94fad100db64f1ab6c4a2992ddee44c8
DiRaiks <t.fane@mail.ru>
2019-03-05 01:51:53 +0300

    remove comments

--- www/gamecenter.js
+++ www/gamecenter.js
@@ -1,50 +1,12 @@
 
 var exec = require("cordova/exec");
 
-// var GameCenter = function () {
-//     this.name = "GameCenter";
-// };
-
-// GameCenter.prototype.auth = function (success, failure) {
-//     exec(success, failure, "GameCenter", "auth", []);
-// };
-//
-// GameCenter.prototype.generateIdentityVerification = function (success, failure) {
-//     exec(success, failure, "GameCenter", "generateIdentityVerification", []);
-// };
-//
-// GameCenter.prototype.getPlayerImage = function (success, failure) {
-//     exec(success, failure, "GameCenter", "getPlayerImage", []);
-// };
-//
-// GameCenter.prototype.submitScore = function (success, failure, data) {
-//     exec(success, failure, "GameCenter", "submitScore", [data]);
-// };
-//
-// GameCenter.prototype.showLeaderboard = function (success, failure, data) {
-//     exec(success, failure, "GameCenter", "showLeaderboard", [data]);
-// };
-//
-// GameCenter.prototype.reportAchievement = function (success, failure, data) {
-//     exec(success, failure, "GameCenter", "reportAchievement", [data]);
-// };
-//
-// GameCenter.prototype.resetAchievements = function (success, failure) {
-//     exec(success, failure, "GameCenter", "resetAchievements", []);
-// };
-//
-// GameCenter.prototype.getAchievements = function (success, failure) {
-//     exec(success, failure, "GameCenter", "getAchievements", []);
-// };
-//
-// module.exports = new GameCenter();
-
 module.exports = {
     name: "GameCenter",
     _loggedin: false,
     auth: function (success, failure) {
         var self = this;
-        cordova.exec(function (result) {
+        exec(function (result) {
                 self._loggedin = true;
                 success(result)
             },
